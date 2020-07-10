e269c6d5b576045e004b2fccae6790789df9e78d
Harold Altamiranda Quintero <haroldaltaq1@gmail.com>
2020-07-10 15:11:31 -0500

    first commit
    first commit

---
+++ shopping-service/src/main/java/com/haroldaltaq/store/shopping/repository/InvoiceItemsRepository.java
@@ -0,0 +1,7 @@
+package com.haroldaltaq.store.shopping.repository;
+
+import com.haroldaltaq.store.shopping.entity.InvoiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem,Long> {
+}
